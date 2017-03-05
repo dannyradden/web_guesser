3e1cb89651518ba6f7a70a68ac944a42bb11ae9a
dannyradden <dradden@gmail.com>
2017-03-05 16:40:07 -0700

    Extentions complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,30 +1,77 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :number, rand(100)
+
+
+@@secret = rand(100)
+@@count = 5
+
 
 get '/' do
   guess = params["guess"]
+  @cheat = params["cheat"]
   message = check_guess(guess)
-  erb :index, :locals => {:number => number, :message => message}
+  p @cheat
+  erb :index, :locals => { :message => message, :color => @color }
 end
 
 def number
-  settings.number
+  @@secret
+end
+
+def color(input)
+  @color = input
 end
 
 def check_guess(guess)
-  if guess == nil
-    "Enter a guess"
+  if @@count == 0
+    @@count = 5
+    @@secret = rand(100)
+    "You have lost! A new number has been generated"
+  elsif guess == nil
+    color('white')
+    if @cheat == 'true'
+      "Enter a guess<br>  The SECRET NUMBER is #{@@secret}"
+    else
+      "Enter a guess"
+    end
   elsif guess.to_i > number && guess.to_i > number + 5
-    "Way too high!"
+    @@count -= 1
+    color('red')
+    if @cheat == 'true'
+      "Way too high! <br> The SECRET NUMBER is #{@@secret}"
+    else
+      "Way too high!"
+    end
   elsif guess.to_i > number
-    "Too high!"
+    @@count -= 1
+    color('#E8ADAA')
+    if @cheat == 'true'
+      "Too high! <br> The SECRET NUMBER is #{@@secret}"
+    else
+      "Too high!"
+    end
   elsif guess.to_i < number && 5 + guess.to_i < number
-    "Way too low!"
+    @@count -= 1
+    color('red')
+    if @cheat == 'true'
+      "Way too low! <br> The SECRET NUMBER is #{@@secret}"
+    else
+      "Way too low!"
+    end
   elsif guess.to_i < number
-    'Too low!'
+    @@count -= 1
+    color('#E8ADAA')
+    if @cheat == 'true'
+      "Too Low! <br> The SECRET NUMBER is #{@@secret}"
+    else
+      'Too low!'
+    end
   else
-    "You got it right! \nThe SECRET NUMBER is #{number}"
+    color('green')
+    correct = @@secret
+    @@secret = rand(100)
+    @@count = 5
+    "You got it right! <br> The SECRET NUMBER is #{correct} <br> A new number has been generated."
   end
 end
